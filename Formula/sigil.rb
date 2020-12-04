a06bd12d2eabae47985788e84573e8e21765fea8
goreleaserbot <goreleaser@carlosbecker.com>
2020-12-04 16:25:52 +0000

    Brew formula update for sigil version 0.7.2

--- Formula/sigil.rb
+++ Formula/sigil.rb
@@ -7,11 +7,11 @@ class Sigil < Formula
 
   if OS.mac?
     url "https://github.com/zendesk/sigil/releases/download/0.7.2/sigil_0.7.2_Darwin_x86_64.tar.gz"
-    sha256 "d17b078105c247b4fd5e2fde981fd7f25635525bf866168c88f8a2ccb86b6fdf"
+    sha256 "934b16db4a8bffcb89ff00e0ea3c4aead585f409cc37509c3df9d2f931a7e04b"
   end
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/zendesk/sigil/releases/download/0.7.2/sigil_0.7.2_Linux_x86_64.tar.gz"
-    sha256 "678b1b4c9bf46c7c6deb8f0b7be6f6f670318b0916d246fc8277f66d0e929f6e"
+    sha256 "7ca2ced366d96fd80db5fb379c93f2ed2b6e433d66fb35c57065a27e9796d9aa"
   end
 
   def install
